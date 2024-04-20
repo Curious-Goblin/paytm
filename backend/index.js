4a667edd45ebeffc7c754215c898e32de679889b
Curious-Goblin <poddarsourabh9939@gmail.com>
2024-04-20 21:46:06 +0530

    added the api call for frontend

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const app = express()
 app.use(cors(
     {
-        origin:[""],
+        origin:["https://paytm-frontend-neon.vercel.app"],
         methods:["POST, GET, PUT"],
         credentials:true
     }
