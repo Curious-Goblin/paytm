f4578b91331cd1a845dc1ace95fd4a26da1f356b
Curious-Goblin <poddarsourabh9939@gmail.com>
2024-05-04 09:31:53 +0530

    test

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb+srv://poddarsourabh9939:sourabh123@cluster0.scewquy.mongodb.net/paytmDatabase?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://poddarsourabh9939:AdIoE3Pd0NZkJDk0@paytm-full-stack.8vvjmjm.mongodb.net/?retryWrites=true&w=majority&appName=paytm-full-stack")
 //mongodb+srv://poddarsourabh9939:<password>@cluster0.scewquy.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=Cluster0
-
+//AdIoE3Pd0NZkJDk0
 //mongodb+srv://poddarsourabh9939:Sourabhh%40123@cluster0.scewquy.mongodb.net/
 const UserSchema = new mongoose.Schema({
     username: String,
--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,3 @@ app.use("/api/v1", mainRouter)
 app.listen(3000, () => {
     console.log("server is running at 3000")
 })
-// "dev":"node index.js",
-// ,
-    
-//     "build":"npm i"
