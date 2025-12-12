f585cb1fd9bced142cb28a16f3a267d0426967be
Sourabh Poddar <poddarsourabh9939@gmail.com>
2025-12-12 20:23:40 +0530

    testing deployement error

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,10 @@ const bodyParser = require("body-parser");
 const app = express()
 app.use(cors(
     {
-        origin:["https://paytm-virtual.vercel.app","http://localhost:5173"],
+        // origin:["https://paytm-virtual.vercel.app","http://localhost:5173"],
+        origin: "*",
         methods:["POST, GET, PUT"],
-        credentials:true
+        // credentials:true
     }
 ))
 
